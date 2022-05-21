d1648b496b9f76b119c2fc236ec746f2d3c18f81
dominique <domin@DESKTOP-OT2687Q>
2022-05-21 13:58:13 -0500

    DEV

--- server/schemas/resolvers.js
+++
@@ -1,9 +0,0 @@
-const { AuthenticationError } = require('apollo-server-express');
-const {  } = require('../models');
-const { signToken } = require('../utils/auth');
-
-const resolvers = {
-  
-};
-
-module.exports = resolvers;
--- server/seeders/seeds.js
+++
@@ -1,9 +0,0 @@
-// const faker = require('faker');
-const userSeeds = require('./userSeed.json');
-const thoughtSeeds = require('./thoughtSeed.json');
-const db = require('../config/connection');
-const {  } = require('../models');
-
-db.once('open', async () => {
-  
-});
