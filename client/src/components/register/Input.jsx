872f38627484c174e229fc451e923167fabc68a7
Christine Seddon <noddes@gmail.com>
2022-05-22 18:30:42 -0500

    Added footer component

--- client/src/components/register/Input.jsx
+++ client/src/components/register/Input.jsx
@@ -1,4 +1,4 @@
-const fixedInputClass = "rounded-md appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+const fixedInputClass = 'rounded-md appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm'
 
 export default function Input({
   handleChange,
@@ -13,8 +13,8 @@ export default function Input({
   customClass
 }) {
   return (
-    <div className="my-5">
-      <label htmlFor={labelFor} className="sr-only">
+    <div className='my-5'>
+      <label htmlFor={labelFor} className='sr-only'>
         {labelText}
       </label>
       <input
