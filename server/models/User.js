d63a4dcd2565cf7965f31b7126db84b1d3ca3d17
Zach Davis <zacharydavis92@gmail.com>
2022-05-21 21:16:32 -0500

    'added models'

---
+++ server/models/User.js
@@ -0,0 +1,66 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+@.+\..+/, 'Must match an email address!']
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: 5
+        },
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ],
+        connections: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Jobpost'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        }
+    }
+);
+
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+
+    next();
+});
+
+userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
+
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+userSchema.virtual('connectionsCount').get(function () {
+    return this.connections.length;
+})
+
+const User = model('User', userSchema);
+
+module.exports = User;
