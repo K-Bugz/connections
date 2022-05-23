6396b1d26325d0aaae18d60be15f5391ea9232b3
Christine Seddon <noddes@gmail.com>
2022-05-22 21:03:30 -0500

    imported react to pages

--- client/src/components/register/FormAction.jsx
+++ client/src/components/register/FormAction.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export default function FormAction({
     handleSubmit,
     type = 'Button',
--- client/src/components/register/Login.jsx
+++ client/src/components/register/Login.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useState } from 'react';
 import { loginFields } from '../../constants/formFields';
 import FormAction from './FormAction';
