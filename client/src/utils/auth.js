e2826bebafb6aa611327f6c052ab06b85eebae6c
JoDamaso <darryldamaso@gmail.com>
2022-05-24 17:47:38 -0500

    added 2 mutations and utils in client directory

---
+++ client/src/utils/auth.js
@@ -0,0 +1,53 @@
+import decode from 'jwt-decode';
+
+class AuthService {
+    // retrieve data saved in token
+    getProfile() {
+        return decode(this.getToken());
+    }
+
+    // check if the user is still logged in
+    loggedIn() {
+        // Checks is there is a saved token and it's still valid
+        const token = this.getToken();
+        // use type coersion to check if token is NOT undefined and the token is NOT expired
+        return !!token && !this.isTokenExpired(token);
+    }
+
+    // check if the token has expired
+    isTokenExpired(token) {
+        try {
+            const decoded = decode(token);
+            if (decoded.exp < Date.now() / 1000) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (err) {
+            return false;
+        }
+    }
+
+    // retrieve token from localStorage
+    getToken() {
+        // retrieves token from localStorage
+        return localStorage.getItem('id_token');
+    }
+
+    // set token to localStorage and reload page to homepage
+    login(idToken) {
+        // saves user token to localStorage
+        localStorage.setItem('id_token', idToken);
+
+        window.location.assign('/');
+    }
+
+    // clear token from localStorage and force logout with reload page
+    logout() {
+        // clear user token and profile data from localStorage
+        localStorage.removeItem('id_token');
+        // this reloads page and resets state of application
+        window.location.assign('/');
+    }
+};
+export default new AuthService();
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,6 +2,8 @@ const { AuthenticationError } = require('apollo-server-express');
 const { User, Jobpost } = require('../models');
 const { signToken } = require('../utils/auth');
 
+// need to Auth to add job posts to database.
+
 const resolvers = {
     Query: {
         users: async () => { // get all users
@@ -15,6 +17,33 @@ const resolvers = {
                 .select('-__v -password') // omitting this data from the search
                 .populate()
                 .populate()
+        },
+    },
+    Mutation: {
+        addUser: async (parent, args) => {
+            // created user with args as data
+            const user = await User.create(args);
+
+            const token = signToken(user);
+
+            return { user, token };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                // we say 'Incorrect credentials' to deter hackers and stop them from knowing they may have gotten one thing right.
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const token = signToken(user)
+            return { token, user };
         },
     }
 };
