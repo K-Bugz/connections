df39881798ad97d5ed4b2f8713c9710c6dd77ef9
JoDamaso <darryldamaso@gmail.com>
2022-05-23 19:43:31 -0500

    started typeDefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,22 @@
 const { AuthenticationError } = require('apollo-server-express');
-const {  } = require('../models');
+const { User, Jobpost } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
-  
+    Query: {
+        users: async () => { // get all users
+            return User.find()
+            .select('-__v -pasword')
+            .populate()
+            .populate()
+        },
+        user: async (parent, { username }) => { // get a user by username
+            return User.findOne({ username })
+                .select('-__v -password') // omitting this data from the search
+                .populate()
+                .populate()
+        },
+    }
 };
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,17 +1,37 @@
 const { gql } = require('apollo-server-express');
 
+// save username, title, website, about
+
+
+// All type definitions need to specify what type of data is expected in return, no matter what.
 const typeDefs = gql`
   type User {
-   
+    _id: ID
+    username: String
+    email: String
+
   }
 
+  type Jobpost {
+    _id: ID
+    username: String
+    createdAt: String
+  }
 
   type Auth {
-    
+    token: ID!
+    user: User
+    _id: ID
+    username: String
+    email: String
   }
 
   type Query {
-    
+    users: [User]
+    user(username: String!): User
+
+    jobposts(username: String!): JobPost
+
   }
 
   type Mutation {
@@ -19,4 +39,5 @@ const typeDefs = gql`
   }
 `;
 
+// exporting
 module.exports = typeDefs;
