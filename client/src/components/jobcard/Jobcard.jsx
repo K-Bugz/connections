11bfa876ea56cd717acbac3328c1968f4b374651
Zach Davis <zacharydavis92@gmail.com>
2022-05-28 15:40:04 -0500

    'finished job page'

--- client/src/components/jobcard/Jobcard.jsx
+++ client/src/components/jobcard/Jobcard.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { SAVE_JOB } from '../../utils/mutations';
 
 export default function JobCard( {job} ) {
-    const [saveJob] = useMutation(SAVE_JOB);
-    const handleSaveJob = () => {
-        saveJob({ variables: { id: job._id, isSaved: true}});
+    const [ jobInfo, setJobInfo ] = useState(job);
+    const [saveJobPost] = useMutation(SAVE_JOB);
+    const handleSaveJob =async () => {
+        try {
+            const upDatedJob = await saveJobPost({ variables: { id: job._id, isSaved: true}});
+            const { data } = upDatedJob;
+            setJobInfo(data.saveJob);
+            alert("Save Job!");
+        } catch (err) {
+            console.log(err);
+        }
     }
     return (
         <React.Fragment>
@@ -25,17 +34,18 @@ export default function JobCard( {job} ) {
                             </span>
                             <div className="flex flex-col">
                                 <span className="font-bold text-sm text-black dark:text-white ml-2">
-                                    {job.title}
+                                    {jobInfo.title}
                                 </span>
                                 <span className="text-sm text-gray-500 dark:text-white ml-2">
-                                   {job.company}
+                                   {jobInfo.company}
                                 </span>
                             </div>
                         </div>
                         <div className="flex items-center">
 
-                            {/* Need click to add/unadd from dashboard                        */}
-                            <button className="border p-3 border-gray-200 rounded-full" onClick={handleSaveJob}>
+                            {jobInfo.isSaved? (<p>Is Saved</p>
+                            ) : (
+                                <button className="border p-3 border-gray-200 rounded-full" onClick={handleSaveJob}>
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
                                     width="40"
@@ -47,17 +57,18 @@ export default function JobCard( {job} ) {
                                     <path d="M1728 647q0 22-26 48l-363 354 86 500q1 7 1 20 0 21-10.5 35.5t-30.5 14.5q-19 0-40-12l-449-236-449 236q-22 12-40 12-21 0-31.5-14.5t-10.5-35.5q0-6 2-20l86-500-364-354q-25-27-25-48 0-37 56-46l502-73 225-455q19-41 49-41t49 41l225 455 502 73q56 9 56 46z"></path>
                                 </svg>
                             </button>
+                            )}
                         </div>
                     </div>
                     <div className="flex items-center justify-start my-4 space-x-4">
                         <span className="px-2 py-1 flex items-center text-xs rounded-md font-semibold text-green-500 bg-green-50">
-                            {job.location}
+                            {jobInfo.location}
                         </span>
                         <span className="px-2 py-1 flex items-center text-xs rounded-md text-blue-500 font-semibold bg-blue-100">
-                            {job.timePosted}
+                            {jobInfo.timePosted}
                         </span>
                     </div>
-                    <a href={job.link} target="_blank"><span className="px-2 py-1 flex w-36 mt-4 items-center text-xs rounded-md font-bold text-blue-500 bg-blue-100">
+                    <a href={jobInfo.link} target="_blank" rel="noreferrer"><span className="px-2 py-1 flex w-36 mt-4 items-center text-xs rounded-md font-bold text-blue-500 bg-blue-100">
                         Click to Apply!
                     </span></a>
                 </div>
