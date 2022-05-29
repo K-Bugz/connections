6a84936e387d9d1f3e4fd7900ebedcf1d91e3fbe
Christine Seddon <noddes@gmail.com>
2022-05-29 15:51:02 -0500

    added connected animation

--- client/src/pages/Jobs.jsx
+++ client/src/pages/Jobs.jsx
@@ -4,6 +4,7 @@ import Navbar from '../components/navbar/Navbar';
 import Jobcard from '../components/jobcard/Jobcard';
 import { useQuery } from '@apollo/client';
 import { QUERY_ALLJOBS } from '../utils/queries';
+import Connected from '../components/navbar/Connected'
 
 export default function Jobs() {
     const { loading, data } = useQuery(QUERY_ALLJOBS);
@@ -15,7 +16,7 @@ export default function Jobs() {
 
     const getJobsToPost = async (data) => {
         try {
-            if (!loading){
+            if (!loading) {
                 const { jobPosts } = data;
                 const slicedPost = jobPosts.slice(0, 11);
                 setJobsToPost(slicedPost);
@@ -31,28 +32,20 @@ export default function Jobs() {
                     <h3 className='text-lg leading-6 font-medium text-gray-900 dark:text-white'>
                         Recommended for you
                     </h3>
-                    <div className='w-3/6'>
-                        <p className='text-gray-500 text-sm'>
-                            Get
-                            <span className='text-blue-400 font-bold'>
-                                &nbsp;Connected&nbsp;
-                            </span>
-                            To Your Dream Job
-                        </p>
+                    <div className='w-6/6 text-gray-500 text-sm'>
+                        <p className='bold italic text-blue-400'>&emsp;Get </p> <Connected></Connected>
                     </div>
-                    <div className='w-1/6 text-right'>
-                    </div>
+                    <div className='w-2/6 text-gray-500 text-sm'></div>
                 </div>
-               
             </div>
             {loading ? (
                 <h3>loading...</h3>
             ) : (
-                    <div className='container' >
-                        {jobsToPost && jobsToPost.map(job => {
+                <div className='container' >
+                    {jobsToPost && jobsToPost.map(job => {
                         return <Jobcard job={job} key={job._id} ></Jobcard>
-                        })};
-                    </div>
+                    })};
+                </div>
             )}
         </React.Fragment>
     )
