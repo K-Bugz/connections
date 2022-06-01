a77dd8a503698a85fd41b38b5a40308e11312611
kbugz <kbugusky@gmail.com>
2022-05-31 22:20:33 -0500

    heroku push for the gibhbaio+2 time

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth')
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
