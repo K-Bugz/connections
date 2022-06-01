7dde80a8d192d23ea507d8bac94ac085fea67e70
kbugz <kbugusky@gmail.com>
2022-05-31 22:13:25 -0500

    heroku push for the gibhbaio+1 time

--- server/server.js
+++ server/server.js
@@ -23,6 +23,9 @@ const startApolloServer = async (typeDefs, resolvers) => {
     if (process.env.NODE_ENV === 'production') {
         app.use(express.static(path.join(__dirname, '../client/build')));
     }
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    });
     db.once('open', () => {
         app.listen(PORT, () => {
             console.log(`API server running on port ${PORT}!`);
