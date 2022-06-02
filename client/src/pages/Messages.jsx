45731ab5ec66bb6cf47cfa453ef28084746a7c82
Zach Davis <zacharydavis92@gmail.com>
2022-06-01 20:22:44 -0500

    'fix bugs in Message page'

--- client/src/pages/Messages.jsx
+++ client/src/pages/Messages.jsx
@@ -59,9 +59,9 @@ export default function Messages() {
                         xmlns="http://www.w3.org/2000/svg"
                       >
                         <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
                           d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
                         ></path>
                       </svg>
